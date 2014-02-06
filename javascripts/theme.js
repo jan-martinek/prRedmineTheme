ecf7750398beaf0410f7b5cf3b47aadb8b08d3bb
Jan Martinek <jan.martinek@proofreason.com>
2014-02-06 13:08:23 +0100

    Auto focus on search input
    Breaking workflows since 1993

--- javascripts/theme.js
+++ javascripts/theme.js
@@ -152,6 +152,10 @@ var ProofReasonRedmineTheme = {
       // header links
       $('#header h1').prepend('<a class="go-to-my-issues" href="https://redmine.proofreason.com/issues?query_id=135">My issues</a><a class="go-to-projects" href="/projects">Projects</a>');
       //standard link for my issues: /issues?assigned_to_id=me&set_filter=1&sort=priority%3Adesc%2Cupdated_on%3Adesc
+
+      if ($(window).width() > 640) {
+        $('#q').focus();
+      }
     }
   },
 
